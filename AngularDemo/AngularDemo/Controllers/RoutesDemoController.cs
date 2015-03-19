4034b28421403f8636f23a00dbf8e282a9131d51
qianhaopower <qianhaopower@gmail.com>
2015-03-19 12:01:15 +1100

    AngularDemo

---
+++ AngularDemo/AngularDemo/Controllers/RoutesDemoController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace AngularDemo.Controllers
+{
+    public class RoutesDemoController : Controller
+    {
+        public ActionResult One()
+        {
+            return View();
+        }
+
+        public ActionResult Two(int donuts =1)
+        {
+            ViewBag.Donuts = donuts;
+            return View();
+        }
+        [Authorize]
+        public ActionResult Three()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularDemo/AngularDemo/Scripts/AngularDemo.js
@@ -0,0 +1,54 @@
+﻿var AngularDemo = angular.module('AngularDemo', ['ngRoute']);
+
+AngularDemo.controller('LandingPageController', LandingPageController);
+AngularDemo.controller('LoginController', LoginController);
+AngularDemo.controller('RegisterController', RegisterController);
+
+AngularDemo.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+
+
+AngularDemo.factory('LoginFactory', LoginFactory);
+
+AngularDemo.factory('RegistrationFactory', RegistrationFactory);
+
+//var configFunction = function ($routeProvider) {
+//    $routeProvider.
+//        when('/routeOne', {
+//            templateUrl: 'routesDemo/one'
+//        })
+//        .when('/routeTwo', {
+//            templateUrl: 'routesDemo/two'
+//        })
+//        .when('/routeThree', {
+//            templateUrl: 'routesDemo/three'
+//        });
+//}
+//configFunction.config (['$routeProvider', configFunction]);
+
+//AngularDemo.config(configFunction);
+
+
+AngularDemo.config(['$routeProvider','$httpProvider',
+function ($routeProvider,$httpProvider) {
+      $routeProvider.
+     when('/routeOne', {
+         templateUrl: 'routesDemo/one'
+     })
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) {
+                return '/routesDemo/two?donuts=' + params.donuts;
+            }
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        })
+         .when('/login', {
+             templateUrl: '/Account/Login',
+             controller: LoginController
+         })
+          .when('/register', {
+              templateUrl: '/Account/Register',
+              controller: RegisterController
+          });
+      $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+  }]);
